6010a68275be5b853accf76be112bb0c1eddbe94
Kostiantyn Zhereb <zhereb@gmail.com>
2020-09-15 13:11:53 +0300

    Example of SUBCASE
    Similar to SECTION from Catch framework

--- code-examples/lab_k29_15_09_2020_doctest.cpp
+++ code-examples/lab_k29_15_09_2020_doctest.cpp
@@ -8,6 +8,7 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include "doctest.h"
 #include <cassert>
+#include <vector>
 
 
 int factorial(int number) {
@@ -23,3 +24,22 @@ TEST_CASE("testing the factorial function") {
     CHECK(factorial(10) == 3628800);
 }
 
+TEST_CASE("vectors can be sized and resized") {
+    std::vector<int> v(5);
+
+    REQUIRE(v.size() == 5);
+    REQUIRE(v.capacity() >= 5);
+
+    SUBCASE("adding to the vector increases it's size") {
+        v.push_back(1);
+
+        CHECK(v.size() == 6);
+        CHECK(v.capacity() >= 6);
+    }
+    SUBCASE("reserving increases just the capacity") {
+        v.reserve(6);
+
+        CHECK(v.size() == 5);
+        CHECK(v.capacity() >= 6);
+    }
+}
