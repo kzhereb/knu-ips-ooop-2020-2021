ac5a673c93fa46f0834989b61f63aaac78c0754a
Kostiantyn Zhereb <zhereb@gmail.com>
2020-09-15 15:51:52 +0300

    Computed property

--- code-examples/lab_k28c_15_09_20_oop.cpp
+++ code-examples/lab_k28c_15_09_20_oop.cpp
@@ -18,11 +18,17 @@ private:
 public:
 	Human(double height, double weight, std::string name): height{height}, weight{weight}, name{name} { }
 	double get_height() { return this->height;}
+	bool is_tankist() { return height <= 170; }
 };
 
 TEST_CASE("creating human and accessing properties") {
 	Human someone(170, 50, "Yehor");
 	CHECK(someone.get_height() == 170);
+	CHECK(someone.is_tankist());
+
+	Human someone2(171, 50, "Andrii");
+	CHECK(someone2.get_height() == 171);
+	CHECK_FALSE(someone2.is_tankist());
 }
 
 
