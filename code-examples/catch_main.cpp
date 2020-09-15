dac852deaa3eb69f0fa10aa3d645e121c3127bd1
Kostiantyn Zhereb <zhereb@gmail.com>
2020-09-15 14:59:35 +0300

    Catch main in separate file, explicit definition

---
+++ code-examples/catch_main.cpp
@@ -0,0 +1,21 @@
+/*
+ * catch_main.cpp
+ *
+ *  Created on: Sep 15, 2020
+ *      Author: KZ
+ */
+
+#define CATCH_CONFIG_RUNNER
+#include "catch.hpp"
+
+int main( int argc, char** argv ) {
+  // global setup...
+
+  int result = Catch::Session().run( argc, argv );
+
+  // global clean-up...
+
+  return result;
+}
+
+
