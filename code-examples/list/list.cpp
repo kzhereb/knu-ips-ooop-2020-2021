817a2af89b6af18996318803c39eeedc76597c91
Kostiantyn Zhereb <zhereb@gmail.com>
2020-09-22 08:23:14 +0300

    Another constructor that sets all fields

--- code-examples/list/list.cpp
+++ code-examples/list/list.cpp
@@ -12,11 +12,19 @@ struct ListNode {
 	ListNode* prev;
 	ListNode* next;
 	ListNode(int value): value{value} {}
+	ListNode(int value, ListNode* prev, ListNode* next): value{value}, prev{prev}, next{next} {}
 };
 
 TEST_CASE("[list] - creating list nodes") {
 	ListNode node{123};
 	CHECK(node.value == 123);
+
+	ListNode node2{456, &node, nullptr};
+	CHECK(node2.value == 456);
+	CHECK(node2.prev == &node);
+	CHECK(node2.prev->value == 123);
+	CHECK(node2.next == nullptr);
+
 }
 
 
