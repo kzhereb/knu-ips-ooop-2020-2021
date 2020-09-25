d2e6b8e2f22a610940d3d28d07d0a93898378c50
Kostiantyn Zhereb <zhereb@gmail.com>
2020-09-25 15:52:09 +0300

    Compiler also supports .cxx extension for source file
    But using different extensions in the same project is not recommended

---
+++ code-examples/lab_k29_25_09_20.cxx
@@ -0,0 +1,15 @@
+/*
+ * lab_k29_25_09_20.cxx
+ *
+ *  Created on: Sep 25, 2020
+ *      Author: KZ
+ */
+
+#include "doctest.h"
+
+TEST_CASE("just to see that it compiles") {
+	CHECK(1+2 == 3);
+}
+
+
+
