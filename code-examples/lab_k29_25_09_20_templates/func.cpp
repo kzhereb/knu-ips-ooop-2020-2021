55fb8ae6e53e7fad8b1666ef68ab80561cd277ee
Kostiantyn Zhereb <zhereb@gmail.com>
2020-09-25 15:16:57 +0300

    Example template function, using doctest::Approx
    Can't compare floating-point numbers using ==, need to take into account
    possible rounding errors

---
+++ code-examples/lab_k29_25_09_20_templates/func.cpp
@@ -0,0 +1,21 @@
+/*
+ * func.cpp
+ *
+ *  Created on: Sep 25, 2020
+ *      Author: KZ
+ */
+
+#include "../doctest.h"
+
+template<typename T>
+T square(T value) {
+	return value*value;
+}
+
+TEST_CASE("[template] - template function") {
+	int result = square(5);
+	CHECK(result == 25);
+
+	CHECK(square<double>(0.5) == 0.25); // power of 2 - can compare exactly
+	CHECK(square<double>(0.1) == doctest::Approx(0.01));
+}
