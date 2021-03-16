19dd4dc3ab92d05af091ae34c006cf1cb4169796
Kostiantyn Zhereb <zhereb@gmail.com>
2021-03-16 13:55:05 +0200

    Example of builder pattern for creatures

---
+++ code-examples/patterns/lab_k28a_2021_03_16_builder.cpp
@@ -0,0 +1,50 @@
+/*
+ * lab_k28a_2021_03_16_builder.cpp
+ *
+ *  Created on: Mar 16, 2021
+ *      Author: KZ
+ */
+
+#include "creature.h"
+#include "creature_factory.h"
+
+#include "../doctest.h"
+
+
+class CreatureBuilder {
+private:
+	Creature* creature;
+public:
+	CreatureBuilder(Creature* creature) : creature{creature} {}
+
+	void recolor(std::string color) {
+		creature->add_modifier(new ColorModifier(color));
+	}
+
+	void make_hostile() {
+		creature->add_modifier(new HostileModifier);
+	}
+
+	void arm_with_weapon() {
+		creature->add_modifier(new WeaponModifier);
+	}
+
+	Creature* get_creature() {
+		return creature;
+	}
+};
+
+TEST_CASE("Modifying creatures with builder") {
+	Creature* unmodified = CreatureFactory::get_instance().create_creature("Priest");
+	CHECK(unmodified->is_hostile() == false);
+	CHECK(unmodified->get_color() == std::string(""));
+
+	CreatureBuilder builder{unmodified};
+	builder.make_hostile();
+	Creature* priest = builder.get_creature();
+
+	CHECK(priest->is_hostile() == true);
+	CHECK(priest->get_color() == std::string("red"));
+}
+
+
