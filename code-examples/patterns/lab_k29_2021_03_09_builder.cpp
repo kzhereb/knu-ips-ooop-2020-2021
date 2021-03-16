a7c53a18a8e65197fdd2d81942fe5f2f671945ed
Kostiantyn Zhereb <zhereb@gmail.com>
2021-03-16 11:40:15 +0200

    Example of chained/fluent API for builder
    Calling as builder.buildPart1().buildPart2()...

--- code-examples/patterns/lab_k29_2021_03_09_builder.cpp
+++ code-examples/patterns/lab_k29_2021_03_09_builder.cpp
@@ -17,18 +17,22 @@ public:
 	static std::string html_encode(std::string text) {
 		return text; //TODO: implement me!
 	}
-	void add_text(std::string text) {
+	HtmlBuilder& add_text(std::string text) {
 		current_html << "<p>"<< html_encode(text)<<"</p>"<<std::endl;
+		return *this;
 	}
-	void add_title(std::string text, int level) {
+	HtmlBuilder& add_title(std::string text, int level) {
 		std::string header_tag = "h"+std::to_string(level);
 		current_html << "<"<<header_tag<<">"<<html_encode(text)<<"</"<<header_tag<<">"<<std::endl;
+		return *this;
 	}
-	void add_link(std::string url, std::string text) {
+	HtmlBuilder& add_link(std::string url, std::string text) {
 		current_html << "<a href='"<<url<<"'>"<<html_encode(text)<<"</a>"<<std::endl;
+		return *this;
 	}
-	void add_image(std::string img_url, std::string alt_text) {
+	HtmlBuilder& add_image(std::string img_url, std::string alt_text) {
 		current_html << "<img src='"<<img_url<<"' alt='"<<html_encode(alt_text)<<"'>"<<std::endl;
+		return *this;
 	}
 
 	std::string get_html() {
@@ -38,10 +42,18 @@ public:
 
 TEST_CASE("building HTML using Builder pattern") {
 	HtmlBuilder builder;
-	builder.add_title("New Cars", 1);
-	builder.add_title("Buy now!", 2);
-	builder.add_text("Buy our new cars at considerable discount");
-	builder.add_link("https://carsales.com/buy", "Buy a new car");
+	SUBCASE("using regular API - each call as separate statement") {
+		builder.add_title("New Cars", 1);
+		builder.add_title("Buy now!", 2);
+		builder.add_text("Buy our new cars at considerable discount");
+		builder.add_link("https://carsales.com/buy", "Buy a new car");
+	}
+	SUBCASE("using fluent/chaining API - multiple calls chained together as a single statement") {
+		builder.add_title("New Cars", 1)
+				.add_title("Buy now!", 2)
+				.add_text("Buy our new cars at considerable discount");
+		builder.add_link("https://carsales.com/buy", "Buy a new car");
+	}
 	CHECK(builder.get_html() == std::string(
 			"<h1>New Cars</h1>\n"
 			"<h2>Buy now!</h2>\n"
