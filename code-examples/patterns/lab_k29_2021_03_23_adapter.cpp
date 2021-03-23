2df6a64e6a4b210046fc824320ed90409c580303
Kostiantyn Zhereb <zhereb@gmail.com>
2021-03-23 14:37:56 +0200

    Implemented Adapter pattern for std::vector
    Changed name for one method, all others stay the same

--- code-examples/patterns/lab_k29_2021_03_23_adapter.cpp
+++ code-examples/patterns/lab_k29_2021_03_23_adapter.cpp
@@ -11,9 +11,36 @@
 
 #include <memory>
 #include <string>
+#include <vector>
+
+template<typename T>
+class VectorList: public List<T> {
+private:
+	std::vector<T> container;
+public:
+	void append(T value) override {
+		container.push_back(value);
+	}
+	void clear() override {
+		container.clear();
+	}
+	T operator[](std::size_t index) override {
+		return container[index];
+	}
+	std::size_t size() override {
+		return container.size();
+	}
+};
+
 
 TEST_CASE("using list through interface") {
-	std::shared_ptr<List<std::string>> mylist = std::make_shared<DoublyLinkedList<std::string>>();
+	std::shared_ptr<List<std::string>> mylist;
+	SUBCASE("using custom implemented doubly linked list") {
+		mylist = std::make_shared<DoublyLinkedList<std::string>>();
+	}
+	SUBCASE("using adapted std::vector") {
+		mylist = std::make_shared<VectorList<std::string>>();
+	}
 	mylist->append("hello");
 	mylist->append("world");
 	CHECK(mylist->size() == 2);
