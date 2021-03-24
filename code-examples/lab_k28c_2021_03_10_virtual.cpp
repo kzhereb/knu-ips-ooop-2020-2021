7c7bfffa47b830c226410d24776a3ddc4f43ff8d
Kostiantyn Zhereb <zhereb@gmail.com>
2021-03-24 15:54:05 +0200

    Stateful singleton - number of created types is stored in factory
    Because factory is singleton, we will get accurate count

--- code-examples/lab_k28c_2021_03_10_virtual.cpp
+++ code-examples/lab_k28c_2021_03_10_virtual.cpp
@@ -9,6 +9,7 @@
 
 #include <string>
 #include <stdexcept>
+#include <unordered_map>
 
 class Animal {
 public:
@@ -67,8 +68,10 @@ TEST_CASE("using factory function to create animals") {
 }
 
 class AnimalFactory {
+private:
+	std::unordered_map<std::string,int> type_counts;
 protected:
-	AnimalFactory() {}
+	AnimalFactory(): type_counts{} {}
 public:
 	static AnimalFactory& get() {
 		static AnimalFactory instance;
@@ -80,6 +83,11 @@ public:
 	AnimalFactory& operator=(AnimalFactory&&) = delete;
 
 	Animal* create_animal(std::string type) {
+		if (type_counts.find(type) != type_counts.end()) {
+			type_counts[type]++;
+		} else {
+			type_counts[type] = 1;
+		}
 		if (type == "Cat" || type == "Feline") {
 			return new Cat;
 		} else if (type == "Dog" || type == "Canine") {
@@ -87,6 +95,10 @@ public:
 		} else {
 			throw std::invalid_argument("Unknown type: " + type);
 		}
+	}
+
+	int get_type_count(std::string type) {
+		return type_counts[type];
 	}
 
 };
@@ -98,10 +110,17 @@ TEST_CASE("using factory class (singleton) to create animals") {
 	// AnimalFactory* factory_ptr = &(AnimalFactory::get()); // works, but dangerous - what if someone calls delete?
 	AnimalFactory& factory = AnimalFactory::get();
 
+	CHECK(factory.get_type_count("Dog") == 0);
 	Animal* mypet = factory.create_animal("Dog");
 	CHECK(mypet->make_sound() == std::string("Bow-wow-wow!"));
+	CHECK(factory.get_type_count("Dog") == 1);
 	Animal* another = factory.create_animal("Dog");
 	CHECK(another->make_sound() == std::string("Bow-wow-wow!"));
 	CHECK(mypet != another);
+	CHECK(factory.get_type_count("Dog") == 2);
+	Animal* exotic = factory.create_animal("Canine");
+	CHECK(exotic->make_sound() == std::string("Bow-wow-wow!"));
+	CHECK(factory.get_type_count("Dog") == 2);
+	CHECK(factory.get_type_count("Canine") == 1);
 	delete mypet;
 }
