04d65bbb29e7f12af6fd9ba27acfef6373f2293e
Kostiantyn Zhereb <zhereb@gmail.com>
2021-03-30 14:08:36 +0300

    Implemented adapter to change interface of PropMapCreature

--- code-examples/patterns/lab_k28a_2021_03_30_adapter.cpp
+++ code-examples/patterns/lab_k28a_2021_03_30_adapter.cpp
@@ -24,4 +24,45 @@ TEST_CASE("using propmap creatures") {
 	CHECK(warrior.get_value("damage_per_hit") == 3);
 }
 
+class CreatureAdaptsPropMap: public Creature {
+private:
+	PropMapCreature impl;
+public:
+	CreatureAdaptsPropMap(std::string name, int health, bool is_hostile = true): Creature(name, health, is_hostile) {}
+
+	std::string greet() override {
+		return impl.get_value<std::string>("greet");
+	}
+
+	void heal(int health_amount, Creature* target) override {
+		int damage_to_deal = impl.get_value("damage");
+		change_health_for_target(-damage_to_deal, target);
+	}
+
+	Creature* clone(std::string name) override {
+		CreatureAdaptsPropMap* result = new CreatureAdaptsPropMap(name, this->health, this->is_hostile());
+		result->set_additional_prop("damage", impl.get_value<int>("damage"));
+		result->set_additional_prop("greet", impl.get_value<std::string>("greet"));
+		return result;
+	}
+
+	template<typename T>
+	void set_additional_prop(std::string name, T value) {
+		impl.set_value(name, value);
+	}
+};
+
+
+TEST_CASE("using adapted creatures") {
+	CreatureAdaptsPropMap* warrior_prototype = new CreatureAdaptsPropMap("Warrior_proto", 10, true);
+	warrior_prototype->set_additional_prop("damage",3);
+	warrior_prototype->set_additional_prop("greet",std::string("Prepare to die!!!!"));
+
+	Creature* warrior = warrior_prototype->clone("Warrior1");
+	CHECK(warrior->greet() == std::string("Prepare to die!!!!"));
+}
+
+
+
+
 
