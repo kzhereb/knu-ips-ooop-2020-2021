0eede2b7b18af7752f255405eb4e69d8b82dcd5f
Kostiantyn Zhereb <zhereb@gmail.com>
2021-04-23 14:15:56 +0300

    Threaded implementation

--- code-examples/multithreading/lab_k29_2021_04_23_threads.cpp
+++ code-examples/multithreading/lab_k29_2021_04_23_threads.cpp
@@ -8,6 +8,8 @@
 #include "../doctest.h"
 #include <vector>
 #include <functional>
+#include <thread>
+#include <iostream>
 
 std::vector<int> custom_transform_sequential(const std::vector<int>& input, std::function<int(int)> func) {
 	std::size_t size = input.size();
@@ -18,12 +20,47 @@ std::vector<int> custom_transform_sequential(const std::vector<int>& input, std:
 	return result;
 }
 
+std::vector<int> custom_transform_parallel(const std::vector<int>& input, std::function<int(int)> func) {
+	std::size_t size = input.size();
+	std::vector<int> result(size);
+
+	std::size_t num_threads = std::thread::hardware_concurrency();
+	std::vector<std::thread> threads(num_threads);
+	std::size_t thread_size = (size / num_threads)+1;
+	std::size_t start = 0;
+	for (std::size_t i=0; i<num_threads; i++) {
+		threads[i] = std::thread(
+				[=, &input, &result] () {
+						//std::cout<<"worker thread "<<i<<" starts at "<<start<<std::endl;
+						for(std::size_t j = start; j < std::min(start+thread_size, size); j++) {
+							result[j] = func(input[j]);
+						}
+					}
+				);
+		start+= thread_size;
+	}
+	for (std::size_t i=0; i<num_threads; i++) {
+		threads[i].join();
+	}
+
+
+
+	return result;
+}
+
+
 TEST_CASE("custom tranform - sequential and parallel") {
 	std::vector<int> mylist {1, 2, 3, 4, 5};
 
 	std::function<int(int)> square = [](int value) { return value*value; };
 
-	std::vector<int> result = custom_transform_sequential(mylist, square);
+	std::vector<int> result;
+	SUBCASE("sequential") {
+		result = custom_transform_sequential(mylist, square);
+	}
+	SUBCASE("parallel") {
+		result = custom_transform_parallel(mylist, square);
+	}
 
 	CHECK(result == std::vector<int>{1, 4, 9, 16, 25});
 }
